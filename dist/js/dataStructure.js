392de4f0accedb64c4e50ce93d9fc5a33a22daa9
Martin Wortschack <martin.wortschack@beeone.at>
2015-01-06 14:43:36 +0100

    dist folder

--- dist/js/dataStructure.js
+++ dist/js/dataStructure.js
@@ -94,61 +94,11 @@ var SetVis = (function(vis) {
 
     }
 
-    function Table(initializer) {
-        this.$container = $(initializer.container);
-        this.tableClass = initializer.tableClass || "";
-        this.colNames = ["Name", "Degree", "Sets"];
-        this.elements = initializer.elements || [];
-        this.init();
-    }
-
-    Table.prototype = {
-        init: function() {
-            $table = $('<table>')
-                .addClass(this.tableClass)
-                .append('<thead>')
-                .append('<tbody>');
-
-            this.$container.html($table);
-            this.appendTableHead();
-            this.update(this.elements);
-        },
-        appendTableHead: function() {
-            var $thead = this.$container.find('thead'),
-                content = "<tr>";
-            for (var i = 0, len = this.colNames.length; i < len; i++) {
-                content += "<th>" + this.colNames[i] + "</th>";
-            }
-            content += "</tr>"
-            $thead.append(content);
-        },
-        update: function(elements) {
-            this.elements = elements;
-
-            var arr = [];
-            for (var i = 0, len = this.elements.length; i < len; i++) {
-                arr.push("<tr>");
-                arr.push("<td>" + this.elements[i].name + "</td>");
-                arr.push("<td>" + this.elements[i].degree + "</td>");
-                arr.push("<td>" + this.elements[i].sets.join(", ") + "</td>");
-                arr.push("</tr>");
-            }
-
-            this.$container.find('tbody')
-                .empty()
-                .html(arr.join(""));
-        },
-	      clear: function() {
-		        this.$container.find('tbody').empty();
-	      }
-    };
-
     return $.extend(vis, {
         Element: Element,
         Set: Set,
         SubSet: SubSet,
-        Selection: Selection,
-        Table: Table
+        Selection: Selection
     });
 
 })(SetVis || vis);
