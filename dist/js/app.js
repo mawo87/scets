d811f1522705f7f1e599dea092935620a5ebb071
Martin Wortschack <martin.wortschack@beeone.at>
2015-01-12 07:50:26 +0100

    dist folder

--- dist/js/app.js
+++ dist/js/app.js
@@ -7,8 +7,8 @@
 		var parser = new SetVis.Parser();
 
 		parser.loadSet("../data/skillmat.json")
-			//parser.loadSet("../data/movies.json")
-			//parser.loadSet("../data/skillmatrix_final.json")
+		//parser.loadSet("../data/movies.json")
+		//parser.loadSet("../data/skillmatrix_final.json")
 			.done(function(setFile) {
 				parser.parseFile(setFile.data);
 
