3da062ecc6eecf2f1d524d6504048d44c064228b
Martin Wortschack <martin.wortschack@gmail.com>
2015-01-13 18:12:23 +0100

    dist folder

---
+++ dist/js/BinConfigurator.js
@@ -0,0 +1,33 @@
+/**
+ * Created by martinwortschack on 13.01.15.
+ */
+var SetVis = (function(vis) {
+    function BinConfigurator(initializer) {
+        this.container = initializer.container;
+        this.bins = initializer.bins;
+        this.templates = {
+            "binRow": function() {
+                var html = '<div>' +
+                             '<input type="text" value=""/>' +
+                             '<'
+            }
+        };
+  }
+
+    BinConfigurator.prototype = {
+        initialize: function() {
+            for (var i = 0; i < this.bins.length; i++) {
+
+            }
+        }
+    };
+
+    function Bin() {
+
+    }
+
+    vis.BinConfigurator = BinConfigurator;
+
+    return vis;
+
+})(SetVis || {});
