091b7e2d140ace1d8a0eec81de2599bdf04a1447
Martin Wortschack <martin.wortschack@gmail.com>
2015-02-17 19:45:34 +0100

    dist folder

--- dist/js/app.js
+++ dist/js/app.js
@@ -19,94 +19,99 @@
 		}
 	};
 
+	function DataLoader(initializer) {
+		this.url = initializer.url;
+		this.onLoadedCallback = initializer.onLoadedCallback || $.noop();
+	}
+
+	DataLoader.prototype = {
+		load: function() {
+			var self = this;
+			$.getJSON(this.url, function(json) {
+				self.onSetDescriptionLoaded(json);
+			});
+		},
+		onSetDescriptionLoaded: function(setDescription) {
+			var self = this;
+			d3.text(setDescription.file, "text/csv", function(data) {
+				self.getVisData(setDescription, data);
+			});
+		},
+		getVisData: function(setDescription, rawText) {
+			var self = this,
+				url = "http://localhost:8080/api",
+				data = { set_description: setDescription, data: rawText };
+
+			$.ajax({
+				type: "POST",
+				url: url + "/upload",
+				data: data,
+				success: function(resp) {
+					console.log("getVisData success :: ", resp);
+					if (resp && resp.success) {
+						self.onSuccess(resp);
+					}
+				},
+				error: function(err) {
+					console.log("getVisData error :: ", err);
+				}
+			});
+		},
+		onSuccess: function(resp) {
+			if (resp.result && resp.result) {
+				var sets = resp.result.sets,
+					elements = resp.result.elements,
+					tmp = { sets: [], elements: [] };
+
+				//extend the global scats variable and delete sets and elements as we create new instances below
+				$.extend(scats.data, resp.result);
+				delete scats.data.sets;
+				delete scats.data.elements;
+
+				console.log("scats.data :: ", scats.data);
+
+				//create sets
+				for (var i = 0, len = sets.length; i < len; i++) {
+					tmp.sets.push(new scats.Set(sets[i].name, sets[i].count));
+				}
+
+				//create elements
+				for (var i = 0, len = elements.length, e; i < len; i++) {
+					var e = new scats.Element(elements[i].id, elements[i].name);
+					e.sets = elements[i].sets;
+					e.degree = elements[i].degree;
+					tmp.elements.push(e);
+				}
+
+				$.extend(scats.data, tmp);
+
+				console.log("scats.data :: after adding sets and elements ", scats.data);
+
+				if (this.onLoadedCallback) {
+					this.onLoadedCallback.call(this);
+				}
+
+			}
+		}
+	};
+
 	$(function() {
 
-		//var parser = new scats.Parser();
+		$('#loader').show();
+		$('#main').hide();
 
-		(function() {
-			//$.getJSON("../data/skillmatrix_final.json", function(json) {
-			$.getJSON("../data/skillmat.json", function(json) {
-				onSetDescriptionLoaded(json);
-			});
-
-			function onSetDescriptionLoaded(setDescription) {
-				d3.text(setDescription.file, "text/csv", function(data) {
-					getVisData(setDescription, data);
-				});
-			}
-
-			function getVisData(setDescription, rawText) {
-				var url = "http://localhost:8080/api",
-					data = { set_description: setDescription, data: rawText };
-
-				$.ajax({
-					type: "POST",
-					url: url + "/upload",
-					data: data,
-					success: function(resp) {
-						console.log("getVisData success :: ", resp);
-						if (resp && resp.success) {
-							onSuccess(resp);
-						}
-					},
-					error: function(err) {
-						console.log("getVisData error :: ", err);
-					}
-				});
-			}
-
-			function onSuccess(resp) {
-				if (resp.result && resp.result) {
-					var sets = resp.result.sets,
-						elements = resp.result.elements,
-						tmp = { sets: [], elements: [] };
-
-					$.extend(scats.data, resp.result);
-					delete scats.data.sets;
-
-					console.log("scats.data :: ", scats.data);
-
-					//create sets
-					for (var i = 0, len = sets.length; i < len; i++) {
-						tmp.sets.push(new scats.Set(sets[i].name, sets[i].count));
-					}
-
-					//create elements
-					for (var i = 0, len = elements.length, e; i < len; i++) {
-						var e = new scats.Element(elements[i].id, elements[i].name);
-						e.sets = elements[i].sets;
-						e.degree = elements[i].degree;
-						tmp.elements.push(e);
-					}
-
-					$.extend(scats.data, tmp);
-
-					console.log("scats.data :: after adding sets and elements ", scats.data);
-
-					var renderer = new scats.Renderer();
-					renderer.render();
-
-					var renderer = new scats.Renderer();
-					renderer.render();
-
-				}
-			}
-		})();
-
-		/*
-		parser.loadSet("../data/skillmat.json")
-		//parser.loadSet("../data/movies.json")
-		//parser.loadSet("../data/skillmatrix_final.json")
-			.done(function(setFile) {
-				parser.parseFile(setFile.data);
+		var dataLoader = new DataLoader({
+			url: "../data/skillmat.json",
+			onLoadedCallback: function() {
+				$('#loader').fadeOut();
+				$('#main').fadeIn();
 
 				var renderer = new scats.Renderer();
 				renderer.render();
-			})
-			.fail(function(error) {
-				console.log("error ", error);
-			});
-		*/
+			}
+		});
+
+		dataLoader.load();
 
 	});
 
